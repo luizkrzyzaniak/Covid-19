a6f7494dcd398551edbbea7f93f14300a133a3bf
Luiz Krzyzaniak <luiz_krzyzaniak@yahoo.com>
2020-08-14 11:58:55 -0300

    Dropdown Select com lista dos paises pela API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
   MenuItem,
   FormControl,
@@ -7,25 +7,53 @@ import {
 import './App.css';
 
 function App() {
+  const URL_PAISES = "https://disease.sh/v3/covid-19/countries"; // API utilizada
+  const [paises, setPaises] = useState([]);
+  const [pais, setPais] = useState('Mundial');
+
+  useEffect(() => {
+    const getPaisesData = async () => {
+      await fetch(URL_PAISES)
+      .then((response) => response.json())
+      .then((data) => {
+        const paises = data.map((pais => ({
+          name: pais.country, // nome completo vindo da API
+          value: pais.countryInfo.iso2 // sigla do nome vindo da API
+        })));
+        setPaises(paises)
+      });
+    };
+    getPaisesData();
+  }, []);
+
+  const onPaisChange = (event) => {
+    const paisCodigo = event.target.value;
+    setPais(paisCodigo);
+  }
+
   return (
     <div className="App">
       
       {/* Header */}
+      {/* Titulo + Campo de Seleção dropdown */}
       <div className="app__header">
         <h1>COVID 19</h1>
         <FormControl className="app__dropdwon">
           <Select
             variant="outlined"
-            value="abc"
+            onChange={onPaisChange}
+            value={pais}
           >
+            {/* Loop dos paises */}
             <MenuItem value="Mundial">Mundial</MenuItem>
-            <MenuItem value="Mundial">Mundial</MenuItem>
-            <MenuItem value="Mundial">Mundial</MenuItem>
-            <MenuItem value="Mundial">Mundial</MenuItem>
+            {
+              paises.map(pais => (
+                <MenuItem value={pais.value}>{pais.name}</MenuItem>
+              ))
+            }
           </Select>
         </FormControl>
       </div>
-      {/* Titulo + Campo de Seleção dropdown */}
 
       {/* Info */}
       {/* Info */}
